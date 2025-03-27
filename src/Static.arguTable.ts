e0b7be9762af549d48bf24aa845fd25dad9d74ca
saccharin01 <chousik01@gmail.com>
2025-03-27 17:50:50 +0900

    new | Static.arguTable.ts
    클래스 생성자 내에 프라이빗 필드로 선언되어있던 테이블 값을 외부로 모듈화 및, 해당 객체를 as const를 이용해 상수값으로 고정

---
+++ src/Static.arguTable.ts
@@ -0,0 +1,11 @@
+export const translateTable: Record<string, string> = {
+  ko: "안녕하세요",
+  en: "hello!",
+  jp: "こんにちは",
+} as const;
+
+export const arguAliesTable: Record<string, string> = {
+  korea: "ko",
+  english: "en",
+  japan: "jp",
+} as const;

127920047b1f9a49974407aa448538688810b476
saccharin01 <chousik01@gmail.com>
2025-03-27 16:50:16 +0900

    feat | greeting.ts
    생성자 함수 리팩토링 시도.
    switch case 체이닝을 회피하고 프라이빗 필드를 이용하기 위해 생성자 내부에서 이용되는 테이블 생성

--- src/greeting.ts
+++ src/greeting.ts
@@ -1,23 +1,29 @@
 export default class Greeting {
   lang: string;
+  private translateTable: Record<string, string> = {
+    ko: "안녕하세요",
+    en: "hello!",
+    jp: "こんにちは",
+  };
+
+  private arguAliesTable: Record<string, string> = {
+    korea: "ko",
+    english: "en",
+    japan: "jp",
+  };
+
   constructor(lang: string) {
-    this.lang = lang;
+    this.lang = lang.toLowerCase();
     this.hello();
   }
   hello(): void {
-    switch (this.lang) {
-      case "ko":
-      case "korea":
-        console.log("안녕하세요");
-        break;
-      case "en":
-      case "english":
-        console.log("hello");
-        break;
-      case "jp":
-      case "japan":
-        console.log("こんにちは");
-        break;
+    if (this.lang in this.translateTable) {
+      console.log(this.translateTable[this.lang]);
+    } else if (this.lang in this.arguAliesTable) {
+      const normalizeArgument = this.arguAliesTable[this.lang];
+      console.log(this.translateTable[normalizeArgument]);
+    } else {
+      console.log("잘못된 표현입니다");
     }
   }
 }
